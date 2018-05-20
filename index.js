02befa3b13f5b0e59ad952a6d4a2a36807f8e707
Chris Manthey <calined@gmx.de>
2018-05-20 12:03:55 +0200

    added partials and students name form

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get('/', function (req, res) {
     res.render("index");
 })
 
+app.get('/students/', function (req, res) {
+    res.render("students");
+})
+
 app.listen(3000, function () {
     console.log("server started");
 
